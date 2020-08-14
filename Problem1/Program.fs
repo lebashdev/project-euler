07baefa708fd8fda383a264f64604068d0052408
Sebastien Vanryckeghem <svanryckeghem@gmail.com>
2020-08-14 07:44:50 -0400

    First two problems

---
+++ Problem1/Program.fs
@@ -0,0 +1,35 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+[<MemoryDiagnoser>]
+type Solutions () =
+
+    [<Benchmark>]
+    member this.solution1 () =
+
+        // Get the multiples of three and five under 1000.
+        let multipleOfThreeAndFive = seq { for x in 1..999 do if (x % 5) = 0 || (x % 3) = 0 then x }
+
+        // Add them up.
+        let result = multipleOfThreeAndFive |> Seq.sum
+
+        result
+
+    [<Benchmark>]
+    member this.solution2 () =
+
+        // This shoudl be marginally more efficient, sd iy only enumerates
+        // the numbers we'll ultimately keep.
+        let multipleOfthree = seq { for x in 3..3..999 -> x }
+        let mulitpleOfFive = seq { for x in 5..5..999 do if (x % 15) <> 0 then x }
+        let r2 = ((Seq.sum multipleOfthree) + (Seq.sum mulitpleOfFive))
+
+        r2
+
+[<EntryPoint>]
+let main argv =
+    BenchmarkRunner.Run typeof<Solutions> |> ignore
+
+    0 // return an integer exit code
---
+++ Problem2/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// Represents the Fibonacci sequence.
+let fibonacci = Seq.unfold (fun (a, b) -> Some (a, (b, a + b))) (1, 2)
+
+let printInt (x: int) = Console.WriteLine("{0:#,#}", x)
+let isEven (x: int) = (x % 2) = 0
+let under limit x = x < limit
+
+[<EntryPoint>]
+let main argv =
+
+    // Take the even integers from the Fibonacci sequence
+    // that are under 4,000,000 and add them up.
+    // Print the result.
+    fibonacci
+    |> Seq.filter isEven
+    |> Seq.takeWhile (under 4_000_000)
+    |> Seq.sum
+    |> printInt
+
+    0 // return an integer exit code
